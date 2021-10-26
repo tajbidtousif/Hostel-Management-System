05cc1dac9bb07d17283ff3070e3b1d86ded0d312
Shah Tajbid Tousif <86789646+tajbidtousif@users.noreply.github.com>
2021-10-26 22:56:34 +0600

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// ********* This Student  class have a parameterized method called register which is getting student information input from user ***********
+
+public class Student {
+
+    public int batch;
+    public String id, name;
+
+    public void register(ArrayList<Student> stu_list) 
+    {
+
+        Scanner input = new Scanner(System.in);
+        // Student newStudent = new Student();
+        System.out.printf("Enter Your Name: ");
+        name = input.nextLine();
+        System.out.printf("Enter Your ID: ");
+        id = input.nextLine();
+        System.out.printf("Enter Your Batch: ");
+        batch = input.nextInt();
+        input.nextLine();
+
+     
+
+        stu_list.add(this);
+
+    }
+
+    public void showInfo() 
+    {
+        System.out.printf(name + " " + id + " " + batch + "\n");
+    }
+
+}
 
