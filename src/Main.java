98cf74aab7b05ffa6dfbc793feea184c250f40aa
Shuvo Goswami <76766612+shuvogoswami007@users.noreply.github.com>
2021-10-26 23:00:39 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -21,3 +21,17 @@ public class Main {
         
 
     }
+
+    // ************************* Main Menubar of Management System ****************************
+    
+
+    public static void menu() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\t\t\t\t\tWelcome To Hostel Management System");
+        System.out.println("\t\t\t\t\t------------------------------------");
+        System.out.println("1. Registration");
+        System.out.println("2. Food Circle");
+        System.out.println("3. Emergency Doctor Services");
+        System.out.println("4. Safety");
+        System.out.println("5. Registered Members List");
